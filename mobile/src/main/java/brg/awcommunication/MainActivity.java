66887fdbaa1b6a3e7180ffba071516044f64bcb8
Johnny Moralez <moralez@gmail.com>
2015-06-04 12:33:11 -0400

    [JMO] Layout updates and wiring

--- mobile/src/main/java/brg/awcommunication/MainActivity.java
+++ mobile/src/main/java/brg/awcommunication/MainActivity.java
@@ -2,15 +2,29 @@ package brg.awcommunication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private Button sendMessageBtn;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendMessageBtn = (Button)findViewById(R.id.send_message_btn);
+        sendMessageBtn.setOnClickListener(this);
+
+        status = (TextView)findViewById(R.id.status);
     }
 
     @Override
@@ -33,5 +47,17 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.send_message_btn) {
+            sendMessage();
+        }
+    }
+
+    private void sendMessage() {
+        Log.i(TAG, "Sending message to wearable");
+        status.setText(getString(R.string.sending));
     }
 }
--- wear/src/main/java/brg/awcommunication/MainActivity.java
+++ wear/src/main/java/brg/awcommunication/MainActivity.java
@@ -3,11 +3,17 @@ package brg.awcommunication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    private TextView mTextView;
+    private static final String TAG = "MainActivity";
+
+    private Button sendMessageBtn;
+    private TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +23,23 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                sendMessageBtn = (Button) stub.findViewById(R.id.send_message_btn);
+                sendMessageBtn.setOnClickListener(MainActivity.this);
+
+                status = (TextView)stub.findViewById(R.id.status);
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.send_message_btn) {
+            sendMessage();
+        }
+    }
+
+    private void sendMessage() {
+        Log.i(TAG, "Sending message to device");
+        status.setText(getString(R.string.sending));
     }
 }
